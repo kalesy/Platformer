12789634ac659eb254d1d5016a1a87bbd6ffbc91
kalesy <285361731@qq.com>
2022-10-02 15:05:29 +0800

    Stomping on enemy, killing enemy and die by touching enemy( bug: kill by enmey also triggers stomp, cause no check is done in play if it is a stomp or a touch

--- src/actor/Enemy.gd
+++ src/actor/Enemy.gd
@@ -1,10 +1,17 @@
 extends Actor
 
 func _ready():
-    self.velocity.x = -speed.x
+	self.velocity.x = -speed.x
 
 func _physics_process(delta):
-    velocity.y += gravity * delta
-    if is_on_wall():
-        self.velocity.x *= -1.0
-    self.velocity.y = move_and_slide(self.velocity, self.FLOOR_NORMAL).y
+	velocity.y += gravity * delta
+	if is_on_wall():
+		self.velocity.x *= -1.0
+	self.velocity.y = move_and_slide(self.velocity, self.FLOOR_NORMAL).y
+
+func _on_StompDetector_body_entered(body:Node):
+	if body.global_position.y > $StompDetector.global_position.y:
+		return
+	queue_free()
+	#$CollisionShape2D.disabled = true
+	
--- src/actor/Player.gd
+++ src/actor/Player.gd
@@ -1,10 +1,12 @@
 extends Actor
 
+export var stomp_impulse = 1000.0
+
 func _physics_process(delta: float) -> void:
 	#Input.get_action_strength this function returns the float result of the action move occurs
 	# this is the integer version i think
 	var is_jump_interrupted = Input.is_action_just_released("jump") and velocity.y < 0.0
-	var direction = get_direction()
+	var direction = self.get_direction()
 	self.velocity = calculate_move_velocity(self.velocity, direction, delta, is_jump_interrupted)
 	self.velocity = move_and_slide(self.velocity, self.FLOOR_NORMAL)
 
@@ -28,3 +30,15 @@ func calculate_move_velocity(
 	if is_jump_interrupted:
 		velocity.y = 0.0
 	return velocity
+
+func calculate_stomp_velocity(velocity:Vector2) -> Vector2:
+	velocity.y = -self.stomp_impulse
+	return velocity
+
+func _on_EnemyDetector_area_entered(_area:Area2D):
+	self.velocity = self.calculate_stomp_velocity(self.velocity)
+
+
+func _on_EnemyDetector_body_entered(_body:Node):
+	print("You died!")
+	queue_free()
